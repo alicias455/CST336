d5c1ff2647827eb2bf7d8e692156c4a1c605ddcf
Alicia Sandoval <asandovalgarcia@csumb.edu>
2019-10-22 16:51:50 -0700

    lab6

--- public/lab6/app.js
+++ public/lab6/app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+    res.render("venus.html");
+});
+
+app.get("/earth", function(req, res){
+    res.render("earth.html");
 });
 
 app.get("/", function (req, res) {
     res.send("it works!");
 });
 
-app.get("/mercury", function (req, res) {
-    res.send("This will be Mercury web page!");
-});
+// app.get("/mercury", function (req, res) {
+//     res.send("This will be Mercury web page!");
+// });
 
-app.get("/venus", function (req, res) {
-    res.send("This will be Venus web page!");
-});
+// app.get("/venus", function (req, res) {
+//     res.send("This will be Venus web page!");
+// });
 
 // server listener
 app.listen("8081", "0.0.0.0", function () {
