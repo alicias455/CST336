fd102441310c84a2bfed863e2f060431a1c77b28
Alicia Sandoval <asandovalgarcia@csumb.edu>
2019-11-05 15:31:45 -0800

    final lab7

--- public/lab7/app.js
+++ public/lab7/app.js
@@ -8,7 +8,7 @@ const request = require('request');
 app.get("/", async function (req, res) {
     // res.send("it works!");
 
-    let parseData = await getImages("otters");
+    let parseData = await getImages("otters", 'h');
 
     //console.log("parseData: " + parseData);
     //console.dir("parseData: " + parseData); //displays content of object
@@ -21,9 +21,9 @@ app.get("/results", async function (req, res) {
     console.dir(req);
 
     let keyword = req.query.keyword; //gets the value the user typed using GET method
-    // let orient = req.query.orient;
+    let orient = req.query.orient;
 
-    let parseData = await getImages(keyword);
+    let parseData = await getImages(keyword, orient);
     res.render("results", {"images": parseData});
 
     // request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e', function (error, response, body) {
@@ -41,28 +41,43 @@ app.get("/results", async function (req, res) {
     // res.render("results");
 });
 
-function getImages(keyword){
+function getImages(keyword, orient){
     return new Promise(function (resolve, reject) {
-        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=' + keyword, function (error, response, body) {
-            // console.log('error:', error);
-            // console.log('statusCode:', response && response.statusCode);
-            // console.log('body:', body);
+        if(orient == "h"){
+            request(`https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=${keyword}&orientation=horizontal`, function (error, response, body) {
+                // console.log('error:', error);
+                // console.log('statusCode:', response && response.statusCode);
+                // console.log('body:', body);
 
-            if(!error && response.statusCode == 200){
-                let parseData = JSON.parse(body);
-                resolve(parseData);
-                //let randomIndex = Math.floor(Math.random() * parseData.hits.length);
-                // res.send(`<img src='${parseData.hits[randomIndex].largeImageURL}'>`);
-                //res.render("index", {"image": parseData.hits[randomIndex].largeImageURL});
-            }
-            else {
-                reject(error);
-                console.log(response.statusCode);
-                console.log(error);
-            }
+                if(!error && response.statusCode == 200){
+                    let parseData = JSON.parse(body);
+                    resolve(parseData);
+                    //let randomIndex = Math.floor(Math.random() * parseData.hits.length);
+                    // res.send(`<img src='${parseData.hits[randomIndex].largeImageURL}'>`);
+                    //res.render("index", {"image": parseData.hits[randomIndex].largeImageURL});
+                }
+                else {
+                    reject(error);
+                    console.log(response.statusCode);
+                    console.log(error);
+                }
 
-            // console.log(response.statusCode);
-        });
+                // console.log(response.statusCode);
+            });
+        }
+        else{
+            request(`https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=${keyword}&orientation=vertical`, function (error, response, body) {
+                if(!error && response.statusCode == 200){
+                    let parseData = JSON.parse(body);
+                    resolve(parseData);
+                }
+                else {
+                    reject(error);
+                    console.log(response.statusCode);
+                    console.log(error);
+                }
+            });
+        }
     });
 }
 
@@ -72,6 +87,6 @@ app.listen("8081", "0.0.0.0", function () {
 });
 
 //starting server
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("Express server is running...");
-// });
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
